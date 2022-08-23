d4ff444cd70d95f7cd97c8a3668d57a4bbc1d012
Aaron Enyetu <aaronenyetu@gmail.com>
2022-08-23 16:16:26 -0700

    Added user signup and redirecting to the todo page

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
 // import functions and grab DOM elements
+// on form submit
+// get the info from the form
+// make an API request to /api/v1/users with the info
+
+import { signUpUser } from './fetch-utils.js';
 
 // let state
+const signUpForm = document.getElementById('sign-up');
 
 // set event listeners 
   // get user input
   // use user input to update state 
+signUpForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const formData = new FormData(signUpForm);
+    const data = await signUpUser({
+        email: formData.get('email'),
+        password: formData.get('password'),
+        
+    });
+    console.log(data);
+});
   // update DOM to reflect the new state
+
+// if user is logged in -- redirect to /tasks page
+// else stay here
+
